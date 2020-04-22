8988953d328427ed6a4482127ed9d81312172627
Ravi Pandya <pandya@smooth.tech>
2020-04-22 00:01:30 -0400

    Add Transaction Object Class. Implement card view to bind those transaction. Apply Styling to Transaction Card.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,29 +6,86 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Flutter App',
-        home: MyHomePage(),
-     );
+    return MaterialApp(
+      title: 'Weekly Expense App',
+      home: MyHomePage(),
+    );
   }
-
 }
+
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New T-shirts',
+      amount: 39.99,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Weekly Expense App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Card(
-            child: Text('CHART!'),
-            elevation: 5,
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
           ),
-          Card(
-            child: Text('LIST OF TX'),
-          )
+          //Transaction Card
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+                child: Row(
+              children: <Widget>[
+                //Amount
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    tx.amount.toString(),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      //title
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      //Date
+                      Text(
+                        tx.date.toString(),
+                        style: TextStyle(color:Colors.grey),
+                      )
+                    ]),
+              ],
+            ));
+          }).toList())
         ],
       ),
     );
