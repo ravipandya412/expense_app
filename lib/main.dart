531a34f5f9cc2bdc10132851f03821625c6f831f
Ravi Pandya <pandya@smooth.tech>
2020-05-04 23:19:14 -0400

    Add logic to fetch transactions since last 7 days.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
 import 'package:flutter/material.dart';
 import './models/transaction.dart';
 
@@ -50,6 +51,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransaction.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(
+        Duration(days: 7),
+      ))
+      ;
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txAmount) {
     //After fetching value frm text fields, add those values in to object
     final newTransaction = Transaction(
@@ -93,14 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransaction),
           ],
         ),
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -19,7 +19,7 @@ class Chart extends StatelessWidget {
           totalSum = recentTransactions[i].amount;
         }
       }
-      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
     });
   }
 
